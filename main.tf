f789ba107eb7c559d026b50a251b72f47a7b5240
saiflm17 <viratsaikumar17@gmail.com>
2025-05-30 17:24:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "nginx-container"
+  name  = "nginx-container-01"
   ports {
     internal = 80
     external = 1234
